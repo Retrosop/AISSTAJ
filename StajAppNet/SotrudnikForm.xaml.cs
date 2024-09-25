4bb04d776272ae7a29a4a4cd41727d6b4d4364ce
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-09-25 18:15:27 +1000

    STAJ-4 В AppNet создана рабочая форма подключения к базе данных

--- StajAppNet/SotrudnikForm.xaml.cs
+++ StajAppNet/SotrudnikForm.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Printing;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,164 +22,169 @@ namespace StajAppNet
 	/// </summary>
 	public partial class SotrudnikForm : Window
 	{
-		//int IdSelected = 0;
-		//ObservableCollection<Sotrudnik> sotrudniks;
+		int IdSelected = 0;
+		ObservableCollection<SotrudnikLite> sotrudniks;
 		public SotrudnikForm()
 		{
-		//	InitializeComponent();
-		//	sotrudniks = new ObservableCollection<Sotrudnik>();
-		//	sotrudnikList.SelectedIndex = IdSelected;
-		//	using (DataContext db = new DataContext())
-		//	{
-		//		var glist = db.Sotrudnik.ToList();
-		//		foreach (Sotrudnik g in glist)
-		//		{
-		//			sotrudniks.Add(g);
-		//		}
+			InitializeComponent();
+			sotrudniks = new ObservableCollection<SotrudnikLite>();
+			sotrudnikList.SelectedIndex = IdSelected;
+			using (DataContext db = new DataContext())
+			{
+				var glist = db.Sotrudnik.ToList();
+				foreach (SotrudnikLite g in glist)
+				{
+					sotrudniks.Add(g);
+				}
 
-		//		sotrudnikList.ItemsSource = sotrudniks;
-		//		if (IdSelected != 0)
-		//		{
-		//			var t = db.Sotrudnik
-		//				.Where(c => c.Id == (sotrudnikList.SelectedItems[0] as Sotrudnik).Id)
-		//				.FirstOrDefault();
-		//			InputFio.Text = t.Fio;
-		//			InputFioBeforeMarry.Text = t.FioBeforeMarry;
-		//			InputAge.Text = t.Age.ToString();
-		//			InputPol.IsChecked = t.Pol;
-		//			InputMarry.IsChecked = t.Marry;
-		//			InputDater.Text = t.Dater.ToString("dd.MM.yyyy");
-		//		}
-		//	}
+				sotrudnikList.ItemsSource = sotrudniks;
+				if (IdSelected != 0)
+				{
+					var t = db.Sotrudnik
+						.Where(c => c.Id == (sotrudnikList.SelectedItems[0] as SotrudnikLite).Id)
+						.FirstOrDefault();
+					InputFio.Text = t.Fio;
+					InputFioBeforeMarry.Text = t.FioBeforeMarry;
+					InputAge.Text = t.Age.ToString();
+					InputPol.IsChecked = t.Pol;
+					InputMarry.IsChecked = t.Marry;
+					InputDater.Text = t.Dater.ToString("dd.MM.yyyy");
+				}
+			}
 		}
 
 		//Добавление записи
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			//using (DataContext db = new DataContext())
-			//{
-			//	Sotrudnik sotrudnik = new Sotrudnik();
-			//	sotrudnik.Fio = InputFio.Text;
-			//	sotrudnik.FioBeforeMarry = InputFioBeforeMarry.Text;
-			//	sotrudnik.Dater = DateTime.Parse(InputDater.Text);
-			//	sotrudnik.Pol = InputPol.IsChecked;
-			//	sotrudnik.Age = int.Parse(InputAge.Text);
-			//	sotrudnik.Marry = InputMarry.IsChecked;
+			using (DataContext db = new DataContext())
+			{
+				SotrudnikLite sotrudnik = new SotrudnikLite();
+				sotrudnik.Fio = InputFio.Text;
+				sotrudnik.FioBeforeMarry = InputFioBeforeMarry.Text;
+				sotrudnik.Dater = DateTime.Parse(InputDater.Text);
+				sotrudnik.Pol = InputPol.IsChecked;
+				sotrudnik.Age = int.Parse(InputAge.Text);
+				sotrudnik.Marry = InputMarry.IsChecked;
 
-			//	try
-			//	{
-			//		db.Sotrudnik.Add(sotrudnik);
-			//		db.SaveChanges();
-			//	}
-			//	catch (FormatException)
-			//	{
-			//		//MessageBox.Show("{0}: Неправильно введен возраст", InputAge.Text);
-			//	}
-			//	//==================
-			//	sotrudniks.Clear();
-			//	var glist = db.Sotrudnik.ToList();
-			//	foreach (Sotrudnik g1 in glist)
-			//	{
-			//		sotrudniks.Add(g1);
-			//	}
-			//	sotrudnikList.ItemsSource = sotrudniks;
-			//	//==================
+				try
+				{
+					db.Sotrudnik.Add(sotrudnik);
+					db.SaveChanges();
+				}
+				catch (FormatException)
+				{
+					//MessageBox.Show("{0}: Неправильно введен возраст", InputAge.Text);
+				}
+				//==================
+				sotrudniks.Clear();
+				var glist = db.Sotrudnik.ToList();
+				foreach (SotrudnikLite g1 in glist)
+				{
+					sotrudniks.Add(g1);
+				}
+				sotrudnikList.ItemsSource = sotrudniks;
+				//==================
 
-			//}
+			}
 		}
 		//Выделение записи левой кнопкой мыши
 		private void MouseClick(object sender, MouseButtonEventArgs e)
 		{
-			//IdSelected = sotrudnikList.SelectedIndex;
+			IdSelected = sotrudnikList.SelectedIndex;
 
-			//using (var db = new DataContext())
-			//{
-			//	if (sotrudnikList.SelectedIndex == -1)
-			//	{
-			//		MessageBox.Show("Выделите запись");
-			//	} else 
-			//	{
-			//		Sotrudnik t = db.Sotrudnik
-			//						.Where(c => c.Id == (sotrudnikList.SelectedItems[0] as Sotrudnik).Id).FirstOrDefault();
-			//		InputFio.Text = t.Fio;
-			//		InputFioBeforeMarry.Text = t.FioBeforeMarry;
-			//		InputAge.Text = t.Age.ToString();
-			//		InputPol.IsChecked = t.Pol;
-			//		InputMarry.IsChecked = t.Marry;
-			//		InputDater.Text = t.Dater.ToString("dd.MM.yyyy");
-			//	}
-			//}
+			using (var db = new DataContext())
+			{
+				if (sotrudnikList.SelectedIndex == -1)
+				{
+					MessageBox.Show("Выделите запись");
+				}
+				else
+				{
+					SotrudnikLite t = db.Sotrudnik
+									.Where(c => c.Id == (sotrudnikList.SelectedItems[0] as SotrudnikLite).Id).FirstOrDefault();
+					InputFio.Text = t.Fio;
+					InputFioBeforeMarry.Text = t.FioBeforeMarry;
+					InputAge.Text = t.Age.ToString();
+					InputPol.IsChecked = t.Pol;
+					InputMarry.IsChecked = t.Marry;
+					InputDater.Text = t.Dater.ToString("dd.MM.yyyy");
+				}
+			}
 		}
 
 		//Удаление записи
 		private void Button_Click_1(object sender, RoutedEventArgs e)
 		{
-			//if (sotrudnikList.SelectedIndex == -1)
-			//{
-			//	MessageBox.Show("Выделите запись");
-			//}
-			//else
-			//{
-			//	MessageBoxResult dialogResult = MessageBox.Show("Вы действительно хотите удалить запись"
-			//		, "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question);
-			//	if (dialogResult == MessageBoxResult.Yes)
-			//	{
-			//		using (var db = new DataContext())
-			//		{
-			//			Sotrudnik t = db.Sotrudnik
-			//							.Where(c => c.Id == (sotrudnikList.SelectedItems[0] as Sotrudnik).Id).FirstOrDefault();
-			//			db.Sotrudnik.Remove(t);
-			//			db.SaveChanges();
+			if (sotrudnikList.SelectedIndex == -1)
+			{
+				MessageBox.Show("Выделите запись");
+			}
+			else
+			{
+				MessageBoxResult dialogResult = MessageBox.Show("Вы действительно хотите удалить запись"
+					, "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question);
+				if (dialogResult == MessageBoxResult.Yes)
+				{
+					using (var db = new DataContext())
+					{
+						SotrudnikLite t = db.Sotrudnik
+										.Where(c => c.Id == (sotrudnikList.SelectedItems[0] as SotrudnikLite).Id).FirstOrDefault();
+						db.Sotrudnik.Remove(t);
+						db.SaveChanges();
 
-			//			//==================
-			//			sotrudniks.Clear();
-			//			var glist = db.Sotrudnik.ToList();
-			//			foreach (Sotrudnik g1 in glist)
-			//			{
-			//				sotrudniks.Add(g1);
-			//			}
-			//			sotrudnikList.ItemsSource = sotrudniks;
-			//			//==================
-			//		}
-			//	}
-			//}
+						//==================
+						sotrudniks.Clear();
+						var glist = db.Sotrudnik.ToList();
+						foreach (SotrudnikLite g1 in glist)
+						{
+							sotrudniks.Add(g1);
+						}
+						sotrudnikList.ItemsSource = sotrudniks;
+						//==================
+					}
+				}
+			}
 		}
 
 		//Обновление записей
 		private void Button_Click_2(object sender, RoutedEventArgs e)
 		{
-			//if (sotrudnikList.SelectedIndex == -1)
-			//{
-			//	MessageBox.Show("Выделите запись");
-			//}
-			//else
-			//{
-			//	using (var db = new DataContext())
-			//	{
-			//		Sotrudnik t = db.Sotrudnik
-			//						.Where(c => c.Id == (sotrudnikList.SelectedItems[0] as Sotrudnik).Id).FirstOrDefault();
+			if (sotrudnikList.SelectedIndex == -1)
+			{
+				MessageBox.Show("Выделите запись");
+			}
+			else
+			{
+				using (var db = new DataContext())
+				{
+					SotrudnikLite t = db.Sotrudnik
+									.Where(c => c.Id == (sotrudnikList.SelectedItems[0] as SotrudnikLite).Id).FirstOrDefault();
 
-			//		t.Fio = InputFio.Text;
-			//		t.FioBeforeMarry = InputFioBeforeMarry.Text;
-			//		t.Age = int.Parse(InputAge.Text);
-			//		t.Pol = InputPol.IsChecked;
-			//		t.Marry = InputMarry.IsChecked;
-			//		t.Dater = DateTime.Parse(InputDater.Text);
+					t.Fio = InputFio.Text;
+					t.FioBeforeMarry = InputFioBeforeMarry.Text;
+					t.Age = int.Parse(InputAge.Text);
+					t.Pol = InputPol.IsChecked;
+					t.Marry = InputMarry.IsChecked;
+					t.Dater = DateTime.Parse(InputDater.Text);
 
 
-			//		db.SaveChanges();
+					db.SaveChanges();
 
-			//		//==================
-			//		sotrudniks.Clear();
-			//		var glist = db.Sotrudnik.ToList();
-			//		foreach (Sotrudnik g1 in glist)
-			//		{
-			//			sotrudniks.Add(g1);
-			//		}
-			//		sotrudnikList.ItemsSource = sotrudniks;
-			//		//==================
-			//	}
-			//}
+					//==================
+					sotrudniks.Clear();
+					var glist = db.Sotrudnik.ToList();
+					foreach (SotrudnikLite g1 in glist)
+					{
+						sotrudniks.Add(g1);
+					}
+					sotrudnikList.ItemsSource = sotrudniks;
+					//==================
+				}
+			}
+		}
+
+		private void Button_Click_3(object sender, RoutedEventArgs e)
+		{
 		}
 	}
 }
