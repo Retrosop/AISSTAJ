623a0ea18cb386813c899ab20771bb05e60bbc5d
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2024-09-25 16:29:31 +1000

    STAJ - 4 Проверка подключения выполнена на примере TestCoonect

--- TestConnect/DBConnect.cs
+++ TestConnect/DBConnect.cs
@@ -9,9 +9,9 @@ namespace TestConnectDB
 	{
 		public void Add(DataContext db, string fio, DateTime dateroj, int age, bool pol)
 				{
-					//Sotrudnik sotr = new Sotrudnik() { Fio = fio, Age = age, Dater = dateroj, Pol = pol};
-					//db.Sotrudnik.Add(sotr);
-					//db.SaveChanges();
+					SotrudnikLite sotr = new SotrudnikLite() { Fio = fio, Age = age, Dater = dateroj, Pol = pol};
+					db.Sotrudnik.Add(sotr);
+					db.SaveChanges();
 				}
 	}
 	class Program
@@ -24,51 +24,55 @@ namespace TestConnectDB
 			// Добавление
 			using (DataContext db = new DataContext())
 			{
-				//act.Add(db, "Semenov1", DateTime.Now, 34, true);
-				//Sotrudnik sotrudnik1 = new Sotrudnik();
-				//sotrudnik1.Fio = "Sidorov5";
-				//sotrudnik1.Age = 38;
-				//sotrudnik1.Dater = DateTime.Now;
-				//sotrudnik1.Pol = true;
-				//db.Sotrudnik.Add(sotrudnik1);
-				//db.SaveChanges();
+				act.Add(db, "Semenov1", DateTime.Now, 34, true);
+				SotrudnikLite sotrudnik1 = new SotrudnikLite();
+				sotrudnik1.Fio = "Sidorov5";
+			    sotrudnik1.Age = 38;
+				sotrudnik1.Dater = DateTime.Now;
+				sotrudnik1.Pol = true;
+				db.Sotrudnik.Add(sotrudnik1);
+				db.SaveChanges();
 
 
 
-				//Sotrudnik sotrudnik2 = new Sotrudnik();
-				//sotrudnik2.Fio = "Sidorov";
-				//sotrudnik2.Age = 38;
-				//sotrudnik2.Dater = DateTime.Now;
-				//db.Sotrudnik.Add(sotrudnik2);
-				//db.SaveChanges();
+				SotrudnikLite sotrudnik2 = new SotrudnikLite();
+				sotrudnik2.Fio = "Sidorov";
+				sotrudnik2.Age = 38;
+				sotrudnik2.Dater = DateTime.Now;
+				db.Sotrudnik.Add(sotrudnik2);
+				db.SaveChanges();
 
-				//Sotrudnik sotrudnik3 = new Sotrudnik() { Fio = "Petrov", Age = 33, Dater = DateTime.Now, Pol = true};
-				//db.Add(sotrudnik3); 
-				//db.SaveChanges();
+				SotrudnikLite sotrudnik3 = new SotrudnikLite() { Fio = "Petrov", Age = 33, Dater = DateTime.Now, Pol = true};
+				db.Add(sotrudnik3); 
+				db.SaveChanges();
 
 				//Удаление сотрудника по коду PK, например 9
-				//Sotrudnik s1 = db.Sotrudnik.SingleOrDefault(s => s.Id == 9);
-				//if (s1 != null)
-				//{
-				//	//удаляем объект
-				//	db.Sotrudnik.Remove(s1);
-				//	db.SaveChanges();
-				//}
+				SotrudnikLite s1 = db.Sotrudnik.SingleOrDefault(s => s.Id == 9);
+				if (s1 != null)
+				{
+					//удаляем объект
+					db.Sotrudnik.Remove(s1);
+					db.SaveChanges();
+				}
 
-				//Sotrudnik s2 = db.Sotrudnik.SingleOrDefault(s => s.Id == 3);
-				//if (s2 != null)
-				//{
-				//	s2.Fio = "LavrovUpdate";
-				//	db.SaveChanges();
-				//}
+				SotrudnikLite s2 = db.Sotrudnik.SingleOrDefault(s => s.Id == 3);
+				if (s2 != null)
+				{
+					s2.Fio = "LavrovUpdate";
+					db.SaveChanges();
+				}
 
-				// выводим данные после обновления
-				//var sall = db.Sotrudnik.Where(p => p.Fio == "Lavrov");
-				//var sall = db.Sotrudnik.Where(p => EF.Functions.Like(p.Fio.ToLower(), "%id%"));
-				//foreach (var s in sall)
-				//{
-				//	Console.WriteLine($"{s.Id}.{s.Fio} - {s.Age}");
-				//}
+				//выводим данные после обновления
+				var sall = db.Sotrudnik.Where(p => p.Fio == "Lavrov");
+				foreach (var s in sall)
+				{
+					Console.WriteLine($"{s.Id}.{s.Fio} - {s.Age}");
+				}
+				sall = db.Sotrudnik.Where(p => EF.Functions.Like(p.Fio.ToLower(), "%id%"));
+				foreach (var s in sall)
+				{
+					Console.WriteLine($"{s.Id}.{s.Fio} - {s.Age}");
+				}
 				Console.WriteLine("HW");
             }
 		}
--- TestConnect/DBmodel.cs
+++ TestConnect/DBmodel.cs
@@ -5,53 +5,69 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TestConnectDB
 {
-	public class Sotrudnik
-	{
-		[Key]
-		public int Id { get; set; }
-		public string? Fio { get; set; }
-		public string? FioBeforeMarry { get; set; }
-		public bool? Marry { get; set; }
-		public DateTime Dater { get; set; }
-		public bool? Pol { get; set; }
-		public int Age { get; set; }
-	}
+	//public class Sotrudnik
+	//{
+	//	[Key]
+	//	public int Id { get; set; }
+	//	public string? Fio { get; set; }
+	//	public string? FioBeforeMarry { get; set; }
+	//	public bool? Marry { get; set; }
+	//	public DateTime Dater { get; set; }
+	//	public bool? Pol { get; set; }
+	//	public int Age { get; set; }
+	//}
 
 	public class StajLite
 	{
 		[Key]
 		public int Id { get; set; }
-		public Sotrudnik SotrudnikIdLite { get; set; }
+		public SotrudnikLite SotrudnikId { get; set; }
 		public DateTime BeginWorkSotrudnik { get; set; }
 		public DateTime EndWorkSotrudnik { get; set; }
 		public DateTime GosWorkSotrudnik { get; set; }
 	}
-	public class Staj
-	{
-		[Key]
-		public int Id { get; set; }
-		public Sotrudnik SotrudnikId { get; set; }
-		public DateTime BeginWorkSotrudnik { get; set; }
-		public DateTime EndWorkSotrudnik { get; set; }
-		public DateTime GosWorkSotrudnik { get; set; }
-	}
+	//public class Staj
+	//{
+	//	[Key]
+	//	public int Id { get; set; }
+	//	public Sotrudnik SotrudnikId { get; set; }
+	//	public DateTime BeginWorkSotrudnik { get; set; }
+	//	public DateTime EndWorkSotrudnik { get; set; }
+	//	public DateTime GosWorkSotrudnik { get; set; }
+	//}
 
-	public class Report
+	public class ReportLite
 	{
 		[Key]
 		public int Id { get; set; }
-		public Staj StajId { get; set; }
+		public StajLite StajId { get; set; }
 		public string Nomerreport { get; set; }
-		
+
 		public DateTime CreateReport { get; set; }
 	}
-	public class Comment
+	//public class Report
+	//{
+	//	[Key]
+	//	public int Id { get; set; }
+	//	public StajLite StajId { get; set; }
+	//	public string Nomerreport { get; set; }
+		
+	//	public DateTime CreateReport { get; set; }
+	//}
+	public class CommentLite
 	{
 		[Key]
 		public int Id { get; set; }
-		public Sotrudnik SotrudnikId { get; set; }
+		public SotrudnikLite SotrudnikId { get; set; }
 		public string CommentsWork { get; set; }
 	}
+	//public class Comment
+	//{
+	//	[Key]
+	//	public int Id { get; set; }
+	//	public Sotrudnik SotrudnikId { get; set; }
+	//	public string CommentsWork { get; set; }
+	//}
 
 	public class SotrudnikLite
 	{
@@ -68,11 +84,10 @@ namespace TestConnectDB
 
 	public class DataContext : DbContext
 	{
-		public DbSet<SotrudnikLite> SotrudnikLite { get; set; }
-		//public DbSet<Sotrudnik> Sotrudnik { get; set; }
-		public DbSet<StajLite> StajLite { get; set; }
-		//public DbSet<Report> Report { get; set; }
-		//public DbSet<Comment> Comment { get; set; }
+		public DbSet<SotrudnikLite> Sotrudnik { get; set; }
+		public DbSet<StajLite> Staj { get; set; }
+		public DbSet<ReportLite> Report { get; set; }
+		public DbSet<CommentLite> Comment { get; set; }
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
